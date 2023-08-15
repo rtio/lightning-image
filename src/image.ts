5157f11bdafda822a01a4fc7f61a2b2473aeef49
Rafael Meneses <meneses.tio@gmail.com>
2023-08-15 18:18:58 -0300

    Fix Tiff issue when reading the file, also introduce a new concept of retring when short of bytes (#9)

--- src/image.ts
+++ src/image.ts
@@ -19,3 +19,10 @@ export interface FormatHandler {
   getDimensions: (data: DataView) => Dimensions;
   isAnimated: (data: DataView) => boolean;
 }
+
+export class ShortOfBytesError extends Error {
+  constructor(message?: string) {
+    super(message); // 'Error' breaks prototype chain here
+    Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
+  }
+}
